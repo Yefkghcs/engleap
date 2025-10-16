02413b2fdaa73bd8fab9988949236ab6bfb4e028
guojue <guojue@guojuedeMacBook-Pro.local>
2025-10-16 16:48:41 +0800

    feat: add api url

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,11 +1,20 @@
 import axios from "axios";
 
+const API_BASE_URL = window.location.host?.includes('localhost')
+    ? ''
+    : 'https://engleap-server-production.up.railway.app';
+
+const axiosInstance = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true, // 如果需要传递 cookies
+});
+
 export default async function fetch(api: string, options?: { method?: string, data?: object }) {
     const { method = 'GET', data } = options || {};
 
     let func;
-    if (method === 'GET') func = axios.get;
-    else func = axios.post;
+    if (method === 'GET') func = axiosInstance.get;
+    else func = axiosInstance.post;
 
     try {
       const res = await func(api, {
